ec29d0e3a3eef1e1ae2f13ccb7bc7aa64b9081a8
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-09-02 16:13:30 +0200

    add footer component

---
+++ src/tests/Footer.test.tsx
@@ -0,0 +1,11 @@
+import { describe, it } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import Footer from '../components/Footer';
+
+describe('Tests for Footer component', () => {
+  
+  it('renders Footer', () => {
+    render(<Footer />);
+
+  })
+})
