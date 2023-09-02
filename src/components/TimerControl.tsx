23f1c77c7792b802bb8f93555041aee0e5165b72
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-09-02 16:13:46 +0200

    add timer control component

---
+++ src/components/TimerControl.tsx
@@ -0,0 +1,12 @@
+import Button from './Button'
+
+const TimerControl = () => {
+  return (
+    <div>
+      <Button title="play"/>
+      <Button title="reset"/>
+    </div>
+  )
+}
+
+export default TimerControl
---
+++ src/tests/TimerControl.test.tsx
@@ -0,0 +1,12 @@
+import { describe, it } from 'vitest';
+import { render, screen, fireEvent } from '@testing-library/react';
+import TimerControl from '../components/TimerControl';
+
+describe('Tests for TimerControl component', () => {
+  
+  it('renders TimerControl', () => {
+    render(<TimerControl />);
+
+  })
+
+})
